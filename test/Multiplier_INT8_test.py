7fa1ade3c3bab17bc7e7b429d5ef0f1493263a97
js2992 <js2992@cornell.edu>
2025-06-19 11:03:41 -0400

    2025-06-19

--- test/Multiplier_INT8_test.py
+++ test/Multiplier_INT8_test.py
@@ -2,6 +2,62 @@ from TestUtil import *
 
 #===========================================================
 
+INT_MIN = -pow(2, 7)
+INT_MAX = (pow(2, 7) - 1)
+
+def rand_8b_full():
+  return random.randint(INT_MIN, INT_MAX)
+
+def rand_8b_pos():
+  return random.randint(0, INT_MAX)
+
+def rand_8b_neg():
+  return random.randint(INT_MIN, 0)
+
+def rand_8b_small_pos():
+  return random.randint(0, 31)
+
+def rand_8b_small_neg():
+  return random.randint(-31, 0)
+
+def rand_8b_large_pos():
+  return random.randint(32, INT_MAX)
+
+def rand_8b_large_neg():
+  return random.randint(INT_MIN, -32)
+
+def rand_8b():
+  sel = random.randint(0, 6)
+  if(sel == 0):
+    return rand_8b_full()
+  elif(sel == 1):
+    return rand_8b_pos()
+  elif(sel == 2):
+    return rand_8b_neg()
+  elif(sel == 3):
+    return rand_8b_small_pos()
+  elif(sel == 4):
+    return rand_8b_small_neg()
+  elif(sel == 5):
+    return rand_8b_large_pos()
+  else:
+    return rand_8b_large_neg()
+
+def saturate(x):
+  if(x > 127):
+    x = 127
+  if(x < -128):
+    x = -128
+  return x
+
+def signed(x):
+  if(x >= 0):
+    return x
+  else:
+    return (x + (1 << 8))
+
+#===========================================================
+
 async def reset(dut):
   dut.rst.value = 1
   await RisingEdge(dut.clk)
@@ -34,3 +90,100 @@ async def test_case_1_simple(dut):
   await check(dut, 0, 0, 0b00000000, 0b00000000, 0b00000000) #   1 2 3
   await check(dut, 0, 0, 0b00000000, 0b00000000, 0b00000001) #     2 3
   await check(dut, 0, 0, 0b00000000, 0b00000000, 0b00000000) #       3
+
+#===========================================================
+
+@cocotb.test()
+async def test_case_2_directed_istream(dut):
+  clock = init_clock(dut)
+
+  N = 10000
+
+  a = []
+  b = []
+  c = []
+
+  for t in range(N):
+    _a = rand_8b()
+    _b = rand_8b()
+    _c = saturate(_a * _b)
+
+    a.append(signed(_a))
+    b.append(signed(_b))
+    c.append(signed(_c))
+
+  i = 0
+  j = 0
+  t = 0
+
+  out = 0b00000000
+
+  await reset(dut)
+
+  while(j < N):
+    await check (
+      dut, 0, (i < N), 
+      (a[i] if(i < N) else 0),
+      (b[i] if(i < N) else 0), out
+    )
+    
+    i += 1
+    t += 1
+
+    if(t >= 5):
+      out = c[j]
+      j += 1
+
+#===========================================================
+
+@cocotb.test()
+async def test_case_3_random_istream(dut):
+  clock = init_clock(dut)
+
+  N = 10000
+
+  a = []
+  b = []
+  c = []
+  t = []
+
+  for i in range(N):
+    _a = rand_8b()
+    _b = rand_8b()
+    _c = saturate(_a * _b)
+
+    a.append(signed(_a))
+    b.append(signed(_b))
+    c.append(signed(_c))
+    t.append(0)
+  
+  out = 0b00000000
+
+  i = 0
+  j = 0
+  k = 0
+
+  await reset(dut)
+
+  while(j < N):
+    en = ((i < N) & random.randint(0, 1))
+    await check (
+      dut, 0, en,
+      (a[i] if(en) else 0),
+      (b[i] if(en) else 0), out
+    )
+    
+    i += en
+
+    for l in range(N):
+      t[l] += (t[l] > 0)
+    
+    if(k < N):
+      t[k] = en
+      k += en
+
+    if(t[j] == 5):
+      out = c[j]
+      j += 1
+
+#===========================================================
